f3f9c74643f37ac011e7b138745ac650e9d76d3d
Md. Mahmudul Hasan Shohag <imaginativeshohag@yahoo.com>
2018-09-26 00:14:35 +0600

    Result Layout

---
+++ app/src/main/java/com/rakib/initialassessment/CatagoryResultActivity.java
@@ -0,0 +1,14 @@
+package com.rakib.initialassessment;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+
+public class CatagoryResultActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_catagory_result);
+    }
+}
