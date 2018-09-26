21622717b078a3566d0043cc2bdfcbabcb1264da
Rakibul Huda <rakib10rr3@gmail.com>
2018-09-26 19:09:40 +0600

    quiz added

---
+++ app/src/main/java/com/rakib/initialassessment/CatagoryResultActivity.java
@@ -0,0 +1,39 @@
+package com.rakib.initialassessment;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class CatagoryResultActivity extends AppCompatActivity {
+
+    TextView categoryTextView;
+    TextView scoreTextView;
+    Button finishButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_catagory_result);
+
+        categoryTextView = findViewById(R.id.txtCategoryName);
+        scoreTextView = findViewById(R.id.txtChildScore);
+        finishButton = findViewById(R.id.btnFinish);
+
+        String score = String.valueOf(getIntent().getIntExtra("score",0));
+        String categoryName = getIntent().getStringExtra("category");
+
+        categoryTextView.setText(categoryName);
+        scoreTextView.setText(score + " out of 5");
+
+        finishButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+
+    }
+}
---
+++ app/src/main/java/com/rakib/initialassessment/QuizActivity.java
@@ -0,0 +1,84 @@
+package com.rakib.initialassessment;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+
+import com.rakib.initialassessment.data.InitialAssessmentDbHelper;
+import com.rakib.initialassessment.model.Question;
+
+import java.util.List;
+
+public class QuizActivity extends AppCompatActivity {
+
+    List<Question> questionList;
+    int score = 0;
+    int qid = 0;
+    Question currentQ;
+    TextView questionTextView;
+    RadioButton optARadioButton,optBRadioButton,optCRadioButton;
+    Button nextButton;
+
+    Intent intent;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quiz);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        InitialAssessmentDbHelper dbHelper = new InitialAssessmentDbHelper(this);
+        questionList = dbHelper.getAllQuestions();
+        currentQ = questionList.get(qid);
+
+        questionTextView = findViewById(R.id.question_name);
+        optARadioButton = findViewById(R.id.radio_opt1);
+        optBRadioButton = findViewById(R.id.radio_opt2);
+        optCRadioButton = findViewById(R.id.radio_opt3);
+        nextButton = findViewById(R.id.butto_next);
+
+        setQuestionView();
+
+        nextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                RadioGroup radioGroup = findViewById(R.id.radio_group_ques);
+                RadioButton answer = findViewById(radioGroup.getCheckedRadioButtonId());
+                radioGroup.clearCheck();
+                if (currentQ.getCorrect().equals(answer.getText()))
+                {
+                    score++;
+                    Log.d("Score",String.valueOf(score));
+                }
+                if (qid<5)
+                {
+                    currentQ = questionList.get(qid);
+                    setQuestionView();
+                } else {
+                    intent = new Intent(getApplicationContext(),CatagoryResultActivity.class);
+                    intent.putExtra("category",currentQ.getCategory());
+                    intent.putExtra("score", score);
+                    startActivity(intent);
+                    finish();
+                }
+            }
+        });
+    }
+
+    private void setQuestionView() {
+        questionTextView.setText(currentQ.getQuestion());
+        optARadioButton.setText(currentQ.getOptionA());
+        optBRadioButton.setText(currentQ.getOptionB());
+        optCRadioButton.setText(currentQ.getOptionC());
+        qid++;
+    }
+
+}
